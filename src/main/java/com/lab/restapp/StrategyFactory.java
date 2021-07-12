4150d8ec02bb8307270db605d6169aedab26bd1c
Mark <marut.dhungana@gmail.com>
2021-07-12 14:06:48 +0545

    Fixed

--- src/main/java/com/lab/restapp/StrategyFactory.java
+++ src/main/java/com/lab/restapp/StrategyFactory.java
@@ -5,10 +5,33 @@
  */
 package com.lab.restapp;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  *
  * @author Marut
  */
+@Component
 public class StrategyFactory {
+    private Map<StrategyName, Strategy> strategies;
+  
+  @Autowired
+  public StrategyFactory(Set<Strategy> strategySet) {
+     createStrategy(strategySet);
+  }
+  
+  public Strategy findStrategy(StrategyName strategyName) {
+     return strategies.get(strategyName);
+  }
+  private void createStrategy(Set<Strategy> strategySet) {
+      strategies = new HashMap<StrategyName, Strategy>();
+      strategySet.forEach( 
+      strategy ->strategies.put(strategy.getStrategyName(), strategy));
+  }
+    
     
 }
