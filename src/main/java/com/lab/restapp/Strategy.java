c029380612fd4368a6e461d9f75972c0b77222e3
Mark <marut.dhungana@gmail.com>
2021-07-12 13:57:59 +0545

    Not Fixed

---
+++ src/main/java/com/lab/restapp/Strategy.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.restapp;
+
+/**
+ *
+ * @author Marut
+ */
+public interface Strategy {
+    void doSearch();
+    StrategyName getStrategyName();
+}
---
+++ src/main/java/com/lab/restapp/StrategyLinear.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lab.restapp;
+
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Marut
+ */
+@Component
+public class StrategyLinear implements Strategy{
+    @Override
+    public void doSearch(){
+        System.out.println("Searching completed ...");
+    }
+    
+    @Override
+    public StrategyName getStrategyName(){
+    return StrategyName.StrategyLinear;
+    }
+}
